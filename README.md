4451c824a5d5f7092f81572050821ea303e3ff40
Nhu Viet Nguyen <braveryandglory@gmail.com>
2022-07-06 17:40:35 +0200

    2 test-cases

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 Testground experiments 
 ---
 
-Please install testground in order to run some cases here. 
+Please install testground in order to run some cases here
+
+testground plan import --from . --name test-ground
+testground run single --plan=test-ground --testcase=many --builder=docker:generic --runner=local:docker --instances=4 --wait 
+testground run single --plan=test-ground --testcase=one --builder=docker:generic --runner=local:docker --instances=4 --wait 
--- main.go
+++ main.go
@@ -13,48 +13,72 @@ import (
 )
 
 var testcases = map[string]interface{}{
-	"test-sync": run.InitializedTestCaseFn(runSync),
+	"one":  run.InitializedTestCaseFn(oneToMany),
+	"many": run.InitializedTestCaseFn(manyToMany),
 }
 
 func main() {
 	run.InvokeMap(testcases)
 }
 
-func runSync(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
+func oneToMany(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 	client := initCtx.SyncClient
+	id := initCtx.GlobalSeq
 
 	for i := 1; i <= runenv.TestInstanceCount; i++ {
-		err := PubAndSub(ctx, initCtx.GlobalSeq, client, fmt.Sprintf("topic%d", i), runenv)
+		topic := fmt.Sprintf("topic-%d", i)
+		ft := sync.NewTopic(topic, "")
+
+		if strings.Contains(topic, strconv.Itoa(int(id))) {
+			seq, err := client.Publish(ctx, ft, fmt.Sprintf("%d has published to the %s topic", id, topic))
+			if err != nil {
+				return err
+			}
+			runenv.RecordMessage("Seq number - %d", seq)
+		}
+
+		fch := make(chan string)
+		_, err := client.Subscribe(ctx, ft, fch)
 		if err != nil {
 			return err
+		}
+
+		for i := 1; i <= runenv.TestInstanceCount; i++ {
+			f := <-fch
+			runenv.RecordMessage("%d has received the message --> %s", id, f)
 		}
 	}
 	return nil
 }
 
-func PubAndSub(ctx context.Context, id int64, client sync.Client, topic string, runenv *runtime.RunEnv) error {
-	ft := sync.NewTopic(topic, "")
+func manyToMany(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+	client := initCtx.SyncClient
+	id := initCtx.GlobalSeq
 
-	if strings.Contains(topic, strconv.Itoa(int(id))) {
+	for i := 1; i <= runenv.TestInstanceCount; i++ {
+		topic := fmt.Sprintf("topic-%d", i)
+		ft := sync.NewTopic(topic, "")
+
 		seq, err := client.Publish(ctx, ft, fmt.Sprintf("%d has published to the %s topic", id, topic))
 		if err != nil {
 			return err
 		}
 		runenv.RecordMessage("Seq number - %d", seq)
-	}
 
-	fch := make(chan string)
-	_, err := client.Subscribe(ctx, ft, fch)
-	if err != nil {
-		return err
-	}
+		fch := make(chan string)
+		_, err = client.Subscribe(ctx, ft, fch)
+		if err != nil {
+			return err
+		}
 
-	for i := 1; i <= runenv.TestInstanceCount; i++ {
-		f := <-fch
-		runenv.RecordMessage("%d has received the message --> %s", id, f)
+		for i := 1; i <= runenv.TestInstanceCount; i++ {
+			f := <-fch
+			runenv.RecordMessage("%d has received the message --> %s", id, f)
+		}
 	}
-
 	return nil
 }
